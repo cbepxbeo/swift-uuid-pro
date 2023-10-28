ab5506d6e65f8ce03c6b392491933057ce3085d3
cbepxbeo <cbepxbeo@mail.ru>
2023-10-28 22:21:38 +0300

    Create UniversallyUniqueIdentifier+Bytes.swift

---
+++ Sources/UniversallyUniqueIdentifier/UniversallyUniqueIdentifier+Bytes.swift
@@ -0,0 +1,21 @@
+/*
+ 
+ Project: UUIDPro
+ File: UniversallyUniqueIdentifier+Bytes.swift
+ Created by: Egor Boyko
+ Date: 28.10.2023
+ Last Fix: 28.10.2023
+ Version: 1.0.0
+ 
+ Status: #Completed | #Not required
+ 
+ */
+
+extension UniversallyUniqueIdentifier {
+    public typealias Bytes = (
+      UInt8, UInt8, UInt8, UInt8,
+      UInt8, UInt8, UInt8, UInt8,
+      UInt8, UInt8, UInt8, UInt8,
+      UInt8, UInt8, UInt8, UInt8
+    )
+}
