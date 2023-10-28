4b2a2211325703c543021977422fbf8900c07a08
cbepxbeo <cbepxbeo@mail.ru>
2023-10-28 22:21:01 +0300

    Create UniversallyUniqueIdentifier.swift

---
+++ Sources/UniversallyUniqueIdentifier/UniversallyUniqueIdentifier.swift
@@ -0,0 +1,14 @@
+/*
+ 
+ Project: UUIDPro
+ File: UniversallyUniqueIdentifier.swift
+ Created by: Egor Boyko
+ Date: 28.10.2023
+ Last Fix: 28.10.2023
+ Version: 1.0.0
+ 
+ Status: #In progress | #Not decorated
+ 
+ */
+
+public struct UniversallyUniqueIdentifier {}
