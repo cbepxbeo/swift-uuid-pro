4c3ebe9a97d2ee3e6f8aa8666fbb5669a7f1ecd9
cbepxbeo <cbepxbeo@mail.ru>
2023-10-29 21:53:57 +0300

    Create UnsafeMutableBufferPointer+HexOption.swift

---
+++ Sources/Extension/UnsafeMutableBufferPointer/UnsafeMutableBufferPointer+HexOption.swift
@@ -0,0 +1,19 @@
+/*
+ 
+ Project: UUIDPro
+ File: UnsafeMutableBufferPointer+HexOption.swift
+ Created by: Egor Boyko
+ Date: 29.10.2023
+ Last Fix: 29.10.2023
+ Version: 1.0.0
+ 
+ Status: #Completed | #Not required
+ 
+ */
+
+extension UnsafeMutableBufferPointer {
+    enum HexOption {
+        case lowercase
+        case uppercase
+    }
+}
