6da0b311109d0227a5015deab3e7c1120feab037
cbepxbeo <cbepxbeo@mail.ru>
2023-10-29 21:32:04 +0300

    Create UniversallyUniqueIdentifier+Initializers.swift

---
+++ Sources/UniversallyUniqueIdentifier/UniversallyUniqueIdentifier+Initializers.swift
@@ -0,0 +1,26 @@
+/*
+ 
+ Project: UUIDPro
+ File: UniversallyUniqueIdentifier+Initializers.swift
+ Created by: Egor Boyko
+ Date: 29.10.2023
+ Last Fix: 29.10.2023
+ Version: 1.0.0
+ 
+ Status: #Completed | #Not required
+ 
+ */
+
+extension UniversallyUniqueIdentifier {
+    public init?<Bytes>(bytes input: Bytes) where Bytes: Sequence, Bytes.Element == UInt8 {
+        var output = Self.zero.bytes
+        let copied = withUnsafeMutableBytes(of: &output) { bytes in
+            UnsafeMutableBufferPointer(
+                start: bytes.baseAddress.unsafelyUnwrapped.assumingMemoryBound(to: UInt8.self),
+                count: 16
+            ).initialize(from: input).1
+        }
+        guard copied == 16 else { return nil }
+        self.init(bytes: output)
+    }
+}
