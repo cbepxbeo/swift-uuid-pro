7bf0169489d1064747884415ff962925b9da4477
cbepxbeo <cbepxbeo@mail.ru>
2023-10-29 22:13:00 +0300

    Create UniversallyUniqueIdentifier+Method+Serialized.swift

---
+++ Sources/UniversallyUniqueIdentifier/UniversallyUniqueIdentifier+Method+Serialized.swift
@@ -0,0 +1,23 @@
+/*
+ 
+ Project: UUIDPro
+ File: UniversallyUniqueIdentifier+Method+Serialized.swift
+ Created by: Egor Boyko
+ Date: 29.10.2023
+ Last Fix: 29.10.2023
+ Version: 1.0.0
+ 
+ Status: #Completed | #Not required
+ 
+ */
+
+extension UniversallyUniqueIdentifier {
+    public func serialized(
+        option: UnsafeMutableBufferPointer<UInt8>.HexOption,
+        separators: Bool = true
+    ) -> String {
+        .init(unsafeUninitializedCapacity: 32 + (separators ? 4 : 0)) { buffer in
+            self.serialize(into: buffer, option: option, separators: separators)
+        }
+    }
+}
