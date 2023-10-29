ae45eeb59f5e69e8218a472eb510c1ace042eb25
cbepxbeo <cbepxbeo@mail.ru>
2023-10-29 22:36:52 +0300

    Update UniversallyUniqueIdentifier+Method+Serialize.swift
    fix values

--- Sources/UniversallyUniqueIdentifier/UniversallyUniqueIdentifier+Method+Serialize.swift
+++ Sources/UniversallyUniqueIdentifier/UniversallyUniqueIdentifier+Method+Serialize.swift
@@ -19,7 +19,7 @@ extension UniversallyUniqueIdentifier {
     ) -> Int {
         self.unsafeBytes { octets in
             var i = 0
-            let set: Set<Int> = .init([3, 5, 7, 9, 15])
+            let set: Set<Int> = .init([3, 5, 7, 9])
             (0..<16).forEach { position in
                 i = buffer.writeHex(octets[position], at: i, option: option)
                 if separators && set.contains(position) {
