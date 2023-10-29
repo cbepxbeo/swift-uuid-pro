b1ee4db88f60630764d520111fe00dd23e49488f
cbepxbeo <cbepxbeo@mail.ru>
2023-10-29 21:12:25 +0300

    Create Timestamp+Accuracy64.swift

---
+++ Sources/Timestamp/Timestamp+Accuracy64.swift
@@ -0,0 +1,19 @@
+/*
+ 
+ Project: SwiftUIRouter
+ File: Timestamp+Accuracy64.swift
+ Created by: Egor Boyko
+ Date: 29.10.2023
+ Last Fix: 29.10.2023
+ Version: 1.0.0
+ 
+ Status: #In progress | #Not decorated
+ 
+ */
+
+extension Timestamp {
+    public enum Accuracy64 {
+        case _48bit
+        case _64bit
+    }
+}
