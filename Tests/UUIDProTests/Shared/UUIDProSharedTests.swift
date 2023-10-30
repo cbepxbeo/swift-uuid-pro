39acc1a16cb46bfc9ee5b5111043b55101025adb
cbepxbeo <cbepxbeo@mail.ru>
2023-10-30 03:14:38 +0300

    Create UUIDProSharedTests.swift

---
+++ Tests/UUIDProTests/Shared/UUIDProSharedTests.swift
@@ -0,0 +1,14 @@
+/*
+ 
+ Project: UUIDPro
+ File: UUIDProSharedTests.swift
+ Created by: Egor Boyko
+ Date: 28.10.2023
+ 
+*/
+
+import XCTest
+import Foundation
+@testable import UUIDPro
+
+final class UUIDProSharedTests: XCTestCase {}
